cbad86796a5e6808daeea7f0ba1bff0a4650c8cc
Vladddd46 <vladddd46@ukr.net>
2020-06-05 21:06:06 +0300

    client added

--- src/mx_client_process.c
+++
@@ -1,9 +0,0 @@
-#include "uchat.h"
-
-void mx_client_process(int client_socket) {
-    int n;
-    char buffer[256];
-    bzero(buffer,256);
-    n = read(client_socket,buffer,255);
-    write(1, buffer, 255);
-}
