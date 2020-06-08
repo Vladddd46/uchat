eb61157d52ddf60c43555b9b7c58d84c43097ecd
yuriilebid <yuriilebids@gmail.com>
2020-06-08 16:05:18 +0300

    server comments and auditor

--- server/inc/uchat.h
+++ server/inc/uchat.h
@@ -15,5 +15,11 @@ void error(char *msg, int status);
 void mx_client_process(int client_socket);
 char *mx_strnew(int size);
 char* mx_rsa_decode_password(char* str);
+int mx_count_string_size(char* str);
+int mx_get_num_by_index(char* str, int index);
+int mx_count_amount_of_digits(long long int n);
+long long int mx_modexp(long long int base, long long int e, long long int mod);
+long long int mx_get_encrypt(long long int n);
+long long int mx_get_decrypt(long long int e, long long int el);
 
 
---
+++ server/src/mx_count_amount_of_digits.c
@@ -0,0 +1,13 @@
+#include "uchat.h"
+
+/* Return amount of digits in number */
+
+int mx_count_amount_of_digits(long long int n) {
+    int count = 0;
+
+    while (n != 0) {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
