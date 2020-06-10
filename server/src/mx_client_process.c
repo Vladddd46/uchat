23d22d4cab09d4b106dbd0027ba5c975b56d732c
Vladddd46 <vladddd46@ukr.net>
2020-06-10 21:43:03 +0300

    operation_determiner added

--- server/src/mx_client_process.c
+++ server/src/mx_client_process.c
@@ -1,11 +1,31 @@
 #include "server.h"
 
+// Determines type of operation.
+static char *operation_determine(char *buffer) {
+	char **splt_buffer      = mx_strsplit(buffer, '\n');
+	char **operation_line   = mx_strsplit(splt_buffer[0], ' ');
+	char *operation         = mx_string_copy(operation_line[1]);
+
+	mx_del_strarr(&splt_buffer);
+	mx_del_strarr(&operation_line);
+	return operation;
+}
+
+
 void mx_client_process(int client_socket) {
     int n;
     char buffer[256];
     bzero(buffer,256);
     while(1) {
-        n = read(client_socket,buffer,255);
-        printf("%s\n",  buffer);
+        n = read(client_socket, buffer, 255);
+       
+      	char *operation = operation_determine(buffer);
+
+      	if (!strcmp(operation, "login")) {
+      		// login functionality.
+      	}
+
+        // refreshing buffer.
+        bzero(buffer,256);
     }
 }
