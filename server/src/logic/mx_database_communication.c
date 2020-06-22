ff04168c27a8cf2330cd65776d4ea737fb2eeb85
Vladddd46 <vladddd46@ukr.net>
2020-06-22 19:03:49 +0300

    trash removed

--- server/src/logic/mx_database_communication.c
+++ server/src/logic/mx_database_communication.c
@@ -1,24 +1,7 @@
 #include "server.h"
 
-// Determines type of packet.
-static char *packet_type_determiner(char *buffer) {
-    char *packet_type = NULL;
-    cJSON *parsed_str = cJSON_Parse(buffer);
-    char *msg = "Json Parsing error\n";
-    if (parsed_str == NULL) {
-        write(2, msg, (int)strlen(msg));
-        exit(1);
-    }
-    cJSON *type = cJSON_GetObjectItemCaseSensitive(parsed_str, "TYPE");
-    if (cJSON_IsString(type) && (type->valuestring != NULL)) {
-        packet_type = mx_string_copy(type->valuestring);
-    }
-    cJSON_Delete(parsed_str);
-    return packet_type;
-}
-
 /*
- * Receive packet and check, type of this packet. 
+ * Receive packet and determines type of this packet. 
  * Modifies db if it`s needed and forms new packet,
  * which must be send to user (if he/she is online)
  * {to: client_login1; type: login; status_code: success}
@@ -27,7 +10,7 @@ static char *packet_type_determiner(char *buffer) {
 char *mx_database_communication(char *packet) {
     if (!strcmp(packet, ""))
         return NULL;
-    char *packet_type = packet_type_determiner(packet);
+    char *packet_type = get_value_buy_key(packet, "TYPE");
     char *send_back_packet;
 
     if (!strcmp(packet_type, "login_c")) {
