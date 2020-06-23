f4ac1194096b7896839e38a5928481e867d04a0a
yuriilebid <yuriilebids@gmail.com>
2020-06-23 17:34:45 +0300

    Login + Registration

--- server/src/logic/registration_system.c
+++ server/src/logic/registration_system.c
@@ -18,12 +18,37 @@
 //     return "STATUS:false";
 // }
 
+static char* mx_insert_all_args(char* login, char* password, char* nickname) {
+	char* request = mx_strjoin("INSERT INTO USERS (LOGIN, PASSWORD, NICKNAME) VALUES('", login);
+
+    request = mx_strjoin(request, "', '");
+    request = mx_strjoin(request, nickname);
+    request = mx_strjoin(request, "', '");
+    request = mx_strjoin(request, password);
+    request = mx_strjoin(request, "');");
+    // printf("%s", request);
+    return request;
+}
+
+static int mx_add_user(const char *str, char* login, char* password, char* nickname) {
+    sqlite3 *db;
+    char *message_error;
+    int exit = sqlite3_open(str, &db);
+    char* sql = mx_insert_all_args(login, password, nickname);
+
+    exit = sqlite3_exec(db, sql, NULL, 0, &message_error);
+    if(exit != SQLITE_OK) {
+    	printf("Error insert");
+    	sqlite3_free(message_error);
+    }
+    return 0;
+}
 char *registration_system(char *packet) {
     char *login    = get_value_by_key(packet, "LOGIN");
     char *password = get_value_by_key(packet, "PASSWORD");
     char *nickname = get_value_by_key(packet, "NICKNAME");
 
-    // int  return_status     = db_reg_request(login, password, nickname);
+    int  return_status     = mx_add_user("../db_functions/uchat.db", login, password, nickname);
     // char *status           = status_converter(return_status);
     // char *msg              = msg_former(return_status);
 
