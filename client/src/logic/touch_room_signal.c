aab48ab7731ca0b0eb83f81f5f47c089224d2690
Denis Goncharenko <dgoncharen@e1r10p3.unit.ua>
2020-07-11 13:16:55 +0300

    for yura

--- client/src/logic/touch_room_signal.c
+++ client/src/logic/touch_room_signal.c
@@ -1,15 +1,14 @@
 #include "client.h"
 
 void touch_room_signal(GtkWidget *listbox, gpointer data){
-    GtkListBoxRow *g= gtk_list_box_get_selected_row(GTK_LIST_BOX(listbox));
-    int a = gtk_list_box_row_get_index(GTK_LIST_BOX_ROW(g));
-    GList *gl = gtk_container_get_children(GTK_CONTAINER(g));
-    GtkGrid *gr = gl->data;
-   GtkWidget *lab = gtk_grid_get_child_at(gr,1,0);
-   mx_printstr((char *)gtk_label_get_text(GTK_LABEL(lab)));
-    //gtk_list_box_get_selected_row()
-   // mx_printstr((char *)gtk_label_get_text(GTK_LABEL(labellmenu)));
-   // mx_printstr(mx_itoa(a));
+    GtkListBoxRow *selectedrow= gtk_list_box_get_selected_row(GTK_LIST_BOX(listbox));
+    int indexrow = gtk_list_box_row_get_index(GTK_LIST_BOX_ROW(selectedrow));
+    GList *gl = gtk_container_get_children(GTK_CONTAINER(selectedrow));
+    GtkGrid *gridchild = gl->data;
+    GtkWidget *lab = gtk_grid_get_child_at(gridchild,1,0);
+
+    mx_printstr((char *)gtk_label_get_text(GTK_LABEL(lab)));
+    mx_printstr(mx_itoa(indexrow));
     // cJSON *packet = cJSON_CreateObject();
     // char  *packet_str = NULL;
 
