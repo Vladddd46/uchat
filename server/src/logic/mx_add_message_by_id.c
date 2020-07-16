c601c2bb977ac0cd41d91d00b24963142a1ce037
Vladddd46 <vladddd46@ukr.net>
2020-07-16 22:49:49 +0300

    trash removed

--- server/src/logic/mx_add_message_by_id.c
+++ server/src/logic/mx_add_message_by_id.c
@@ -1,12 +1,5 @@
 #include "server.h"
 
-static int mx_callback(void* not_used, int argc, char** argv, char** az_con_name) {
-    for(int i = 0; i < argc; i++) {
-        printf("%s: %s\n", az_con_name[i], argv[i]);
-    }
-    return 0;
-}
-
 static int mx_get_last_message_id(int chat_id) {
     sqlite3 *db = opening_db();
     sqlite3_stmt *res;
@@ -16,8 +9,8 @@ static int mx_get_last_message_id(int chat_id) {
     sprintf(sql, "SELECT MAX(MESSAGEID) FROM MESSAGES WHERE CHATID='%d';", chat_id);
     int exit = sqlite3_prepare_v2(db, sql, -1, &res, 0);
     dberror(db, exit, "Error inserting to table");
-	sqlite3_step(res);
-	if((sqlite3_column_text(res, 0)) != NULL)
+    sqlite3_step(res);
+    if((sqlite3_column_text(res, 0)) != NULL)
         last_message_id = atoi((const char*)sqlite3_column_text(res, 0));
     sqlite3_finalize(res);
     sqlite3_close(db);
@@ -30,9 +23,10 @@ static char* mx_delete_slesh_n(char *str) {
 }
 
 static void mx_add_last_message(int chat_id, char* message, char* time, char* sender) {
-	sqlite3 *db = opening_db();
+    sqlite3 *db = opening_db();
     char *message_error;
     char sql[1000];
+    bzero(sql, 1000);
     int check;
     int message_id = mx_get_last_message_id(chat_id);
 
@@ -51,6 +45,7 @@ static void mx_add_last_message(int chat_id, char* message, char* time, char* se
     printf("\n\nSQL req: %s\n\n", sql);
     check = sqlite3_exec(db, sql, NULL, 0, &message_error);
 
+    printf("===>%s\n", message_error);
     dberror(db, check, "Error inserting to table");
     sqlite3_close(db);
 
@@ -59,12 +54,12 @@ static void mx_add_last_message(int chat_id, char* message, char* time, char* se
 }
 
 static void mx_add_message_with_id(int message_id, int chat_id, char* message) {
-	sqlite3 *db = opening_db();
+    sqlite3 *db = opening_db();
     char *message_error;
     char sql[500];
     int check;
 
-	sprintf(sql, "UPDATE MESSAGES SET MESSAGE = '%s' WHERE MESSAGEID = %d;", message ,message_id);
+    sprintf(sql, "UPDATE MESSAGES SET MESSAGE = '%s' WHERE MESSAGEID = %d;", message ,message_id);
     check = sqlite3_exec(db, sql, NULL, 0, &message_error);
     sqlite3_close(db);
 }
@@ -99,22 +94,29 @@ static char *mx_json_packet_former_from_list(char* message, char* time, char* se
     return packet_str;
 }
 
-char* mx_add_message_by_id(char* packet) {
-    int message_id = atoi(get_value_by_key(packet, "MESSAGEID"));
-    char* time = get_value_by_key(packet, "TIME");
-    char* message = get_value_by_key(packet, "MESSAGE");
-    int chat_id = atoi(get_value_by_key(packet, "CHATID"));
-    char* sender = get_value_by_key(packet, "SENDER");
-    char* to = get_value_by_key(packet, "TO");
+char* mx_add_message_by_id(char *packet) {
+    char *message_id_str = get_value_by_key(packet, "MESSAGEID");
+    char *time           = get_value_by_key(packet, "TIME");
+    char *message        = get_value_by_key(packet, "MESSAGE");
+    char *chat_id_str    = get_value_by_key(packet, "CHATID");
+    char *sender         = get_value_by_key(packet, "SENDER");
+    char *to             = get_value_by_key(packet, "TO");
 
-
+    int message_id = atoi(message_id_str);
+    int chat_id    = atoi(chat_id_str);
 
     if(message_id == 0)
-    	mx_add_last_message(chat_id, message, time, sender);
+        mx_add_last_message(chat_id, message, time, sender);
     else
-    	mx_add_message_with_id(message_id, chat_id, message);
+        mx_add_message_with_id(message_id, chat_id, message);
 
-    char* return_packet = mx_json_packet_former_from_list(message, time, sender, chat_id, to);
+    char *return_packet = mx_json_packet_former_from_list(message, time, sender, chat_id, to);
 
+    free(message_id_str);
+    free(time);
+    free(message);
+    free(chat_id_str);
+    free(sender);
+    free(to);
     return return_packet;
 }
