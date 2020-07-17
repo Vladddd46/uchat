1a7f9399a7307306be01d5b1eec491b80bf8ed41
Vladddd46 <vladddd46@ukr.net>
2020-07-17 13:58:16 +0300

    packet receive func added

--- libs/utils/src/packet_receive.c
+++ libs/utils/src/packet_receive.c
@@ -1,5 +1,10 @@
 #include "utils.h" 
 
+/*
+ * Receives crypted packet from socket.
+ * Decrypts packet and returns it.
+ */
+
 // Read first 8 bytes from packet. (They represent the length of the packet.)
 static int packet_length_determine(int socket) {
     char buf[8];
@@ -13,13 +18,18 @@ static int packet_length_determine(int socket) {
 // Receiving packet from the socket.
 char *packet_receive(int socket) {
     int  packet_len = packet_length_determine(socket);
-    char *packet = mx_strnew(packet_len);
-    int  index = 0;
+    int  index      = 0;
+    char *packet    = mx_strnew(packet_len);
+    char *encrypted_packet;
 
     while(index < packet_len) {
         recv(socket, &packet[index], 1, 0);
         index++;
     }
+
+    // encrypted_packet = mx_rsa_decode(packet);
+    // free(packet);
+    // return encrypted_packet;
     return packet;
 }
 
